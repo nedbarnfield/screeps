bb611ce8fab1fbec6e19169426bb8770e3078f6f
nbarnfield <nedbarnfield@gmail.com>
2022-01-17 08:25:15 +1100

    Tutorial part 2

--- tutorial/main.js
+++ tutorial/main.js
@@ -1,12 +1,20 @@
-import { run } from "./role.harvester";
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
-export function loop() {
-    /** A harvester is a creep with at least one carry, move and work parts.
-     * It can utilise the creep.moveTo, creep.harvest, and creep.transfer
-     * methods to find an energy source, move to it, extract the energy and
-     * finally transfer it to the spawn or other building to power it up. */
-    for(var name in Game.creeps){
+module.exports.loop = function () {
+    /**
+     * The main loop takes a list of all available creeps and performs an
+     * action based on their roles and context. Using the assigned role values
+     * from their memory we can apply different actions to otherwise identical
+     * creeps.
+     * */
+     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        run(creep);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
